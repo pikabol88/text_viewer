c2720dd5bec9d6e5a7df38852f8174264db5bb36
pikabol88 <nena.2000@mail.ru>
2020-12-28 12:26:28 +0300

    updating...

--- TV_v2/README.md
+++
@@ -1,2 +0,0 @@
-# TV_v0.1
- Implementation of a simple text viewer based on WinApi
--- TV_v2/textProcessing.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEXTPROCESSING_H
-#define TEXTPROCESSING_H
-
-#include "components.h"
-#include <windows.h>
-
-void MyTextOut(HDC hdc, hViewer_t *hViewer, PAINTSTRUCT ps);
-
-#endif //TEXTPROCESSING_H
